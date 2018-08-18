54ce9045cdd81e61fe1cffa8a3c697d07620f98b
Jeremy <jeremychristopherryan@gmail.com>
2018-08-18 09:06:30 -0700

    Placeholder images

--- constants.py
+++ constants.py
@@ -1,21 +1,57 @@
 #!/usr/bin/env python
 
+import pygame
+
 ##########################################################
 ################## CHARACTERS AND POSES ##################
 ##########################################################
 
-CHAR_DICT = {"Hero": HERO_POSES,
-    "Benethor": WIZARD_POSES,
-    "Sprite": SPRITE_POSES,
-    "Bandit": BANDIT_POSES,
-    "Dragon": DRAGON_POSES}
 
 HERO_POSES = {"Default": "hero_default.png",
     "Angry": "hero_angry.png",
     "Sad": "hero_sad.png",
-    "Eager": "hero_eager.png"}
+    "Eager": "hero_eager.png",
+    "Annoyed": "hero_annoyed.png",
+    "More annoyed": "hero_more_annoyed.png",
+    "Defeated": "hero_defeated.png"}
 
 WIZARD_POSES = {"Default": "wizard_default.png",
-    "Touched": "wizard_touched.png"}
+    "Touched": "wizard_touched.png",
+    "Hands raised": "wizard_hand_raised.png"}
 
 SPRITE_POSES = {"Default": "sprite_default.png"}
+
+BANDIT_POSES = {}
+
+DRAGON_POSES = {}
+
+CHAR_DICT = {"Hiens": WIZARD_POSES,
+    "Benethir": HERO_POSES,
+    "Sprite": SPRITE_POSES,
+    "Bandit": BANDIT_POSES,
+    "Dragon": DRAGON_POSES}
+
+
+#########################################################
+################# WINDOW AND DISPLAY ####################
+#########################################################
+
+GAME_SIZE = (1920, 1080)
+
+TEXT_BOX_BORDER_X = 70
+TEXT_BOX_BORDER_Y = 70
+TEXT_BOX_WIDTH = GAME_SIZE[0] - 2*TEXT_BOX_BORDER_X
+TEXT_BOX_HEIGHT = 360
+TEXT_BOX_POS = (TEXT_BOX_BORDER_X,
+    GAME_SIZE[1] - TEXT_BOX_BORDER_Y - TEXT_BOX_HEIGHT)
+
+TEXT_BORDER_X = 70
+TEXT_BORDER_Y = 70
+TEXT_POS = (TEXT_BOX_POS[0] + TEXT_BORDER_X,
+    TEXT_BOX_POS[1] + TEXT_BORDER_Y)
+TEXT_WIDTH = TEXT_BOX_WIDTH - 2*TEXT_BORDER_X
+TEXT_HEIGHT = TEXT_BOX_HEIGHT - 2*TEXT_BORDER_Y
+TEXT_SPACING_Y = 60
+READING_SPEED = 40
+
+CHAR_POS = (200, 170)
--- game.py
+++ game.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
 import pygame
+from constants import *
+
+from time import time, sleep
 
 class Game(object):
 
     def __init__(self):
         pygame.mixer.pre_init()
         pygame.init()
-        res = self.prompt_resolution()
-        pygame.display.set_mode(res)
+        self.res = self.prompt_resolution()
+        self.display_screen = pygame.display.set_mode(self.res)
+        self.screen = pygame.Surface(GAME_SIZE)
 
         self.script_path = "script/"
+        self.image_path = "images/"
         self.start_file = "start.txt"
         self.main()
 
     def prompt_resolution(self):
-        resolution = (640, 480)
+        resolution = (1200, 675)
         return resolution
 
     def scene(self):
@@ -49,9 +54,34 @@ class Game(object):
 
     def main(self):
 
-        print(self.parse_script())
+        script = self.parse_script()
+
+        then = time()
+        sleep(0.01)
+
         while True:
-            pass
+
+            now = time()
+            dt = now - then
+            then = now
+
+            pygame.event.pump()
+            pressed = pygame.event.get(pygame.KEYDOWN)
+            if len(pressed):
+                keys = [item.key for item in pressed]
+                print(keys)
+                if 13 in keys:
+                    script.go_to_next()
+
+            self.screen.fill((50, 50, 50))
+            line = script.lines[0]
+            line.update(dt)
+            line.draw(self.screen)
+
+            scaled_down = pygame.transform.scale(self.screen, self.res)
+            self.display_screen.blit(scaled_down, (0, 0))
+            pygame.display.flip()
+
 
 
 class Scene(object):
@@ -62,10 +92,9 @@ class Scene(object):
     def add_lines(self, line_list):
         self.lines += line_list
 
-    def __repr__(self):
-        for item in self.lines:
-            print(item)
-        return("End of script.")
+    def go_to_next(self):
+        self.lines = self.lines[1:]
+
 
 
 class Line(object):
@@ -74,13 +103,82 @@ class Line(object):
         """ Char says text in expr way """
         self.char = char
         self.expr = expr
-        self.text = text
+        self.text = text.strip(" ").strip('"')
+        self.characters_shown = 0
+        self.time = 0
+        self.chars_per_second = READING_SPEED
 
     def __repr__(self):
         return ("%s (%s): %s" % (self.char, self.expr, self.text))
 
-    def draw(self):
+    def draw(self, screen):
+        self.draw_character(screen)
+        self.draw_text_box(screen)
+        self.draw_text(screen)
 
+    def update(self, dt):
+        self.time += dt
+
+    def draw_character(self, screen):
+        expressions = CHAR_DICT[self.char]
+        correct_expression = expressions[self.expr.lower().capitalize()]
+        try:
+            img = pygame.image.load("images/" + correct_expression)
+        except:
+            img = pygame.image.load("images/hero_angry.png")
+
+        screen.blit(img, CHAR_POS)
+
+    def draw_text_box(self, screen):
+
+        surf = pygame.Surface((TEXT_BOX_WIDTH, TEXT_BOX_HEIGHT))
+        surf.set_alpha(140)
+        screen.blit(surf, TEXT_BOX_POS)
+
+    def draw_text(self, screen):
+
+        font = pygame.font.SysFont("Tahoma", 40)
+        self.characters_shown = int(self.time*self.chars_per_second)
+        text_to_show = self.text[:self.characters_shown]
+        text_to_try = text_to_show
+        font_img = font.render(text_to_try, 1, (255, 255, 255))
+
+        text_done = ""
+        text_renders = []
+
+        text_to_try = text_to_try.split()[:]
+        test_img = self.text[len(text_done):]
+        test_img = " ".join(test_img.split()[:len(text_to_try)])
+        text_to_try = " ".join(text_to_try)
+        font_img = font.render(test_img.strip(" ").strip('"'), 1, (255, 255, 255))
+
+        while len(text_done) < len(text_to_show):
+
+            #text_to_try = text_to_try[:].split()
+            test_img = self.text[len(text_done):]
+            test_img = " ".join(test_img.split()[:len(text_to_try.split())])
+            #text_to_try = " ".join(text_to_try)
+            font_img = font.render(test_img.strip(" ").strip('"'), 1, (255, 255, 255))
+
+
+            while font_img.get_width() > TEXT_WIDTH:
+                text_to_try = text_to_try[:-1].split()
+                test_img = self.text[len(text_done):]
+                test_img = " ".join(test_img.split()[:len(text_to_try)])
+                text_to_try = " ".join(text_to_try)
+                font_img = font.render(test_img.strip(" ").strip('"'), 1, (255, 255, 255))
+
+            print(len(text_renders))
+            text_to_try = text_to_try[max(len(text_renders)-1, 0):]
+            font_img = font.render(text_to_try.strip(" ").strip('"'), 1, (255, 255, 255))
+            text_renders.append(font_img)
+            text_done += text_to_try + " "*max(len(text_renders) - 1, 0)
+            text_to_try = text_to_show[len(text_done):]
+
+
+        for i, item in enumerate(text_renders):
+            offset = i*TEXT_SPACING_Y
+            screen.blit(item, (TEXT_POS[0], TEXT_POS[1] + offset))
 
 
 if __name__ == '__main__':
